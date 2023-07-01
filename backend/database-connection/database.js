0a8b7602946db5700f4117b7dbc5b3db7c4bf1cc
Nasim Reza <85151859+nasimreza72@users.noreply.github.com>
2023-07-01 15:18:24 +0600

    Update database.js

--- backend/database-connection/database.js
+++ backend/database-connection/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export function connect() {
     // const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env
-    // add mongoDB password to connect and sometimes it's need to update the IP addresse in MongoDb
+    // add mongoDB password to connect and sometimes it's need to update the IP addresse in MongoDb.
     const connStr = `mongodb+srv://nasim:(password)@cluster0.lrlld.mongodb.net/?retryWrites=true&w=majority`
 
     // Mongoose connection events.
